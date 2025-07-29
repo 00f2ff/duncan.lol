8e380ea642b547499f7ae332b0186b09c520aff4
Duncan McIsaac <dbmcisaac1@gmail.com>
2025-07-28 17:37:38 -0700

    copied notion scripts over but they do not work yet

---
+++ scripts/lib/notion.ts
@@ -0,0 +1,121 @@
+import { Client } from "@notionhq/client";
+import { PageObjectResponse, PartialPageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { notionDatabaseId, notionSecretToken } from "../util/config";
+import { NotionToMarkdown } from "notion-to-md";
+import { DateProperty, MultiSelectProperty, RichTextProperty, SelectProperty, StatusProperty, TitleProperty } from "lib/notionDatabasePropertyTypes";
+
+const notion = new Client({
+  auth: notionSecretToken
+});
+
+export const n2m = new NotionToMarkdown({ notionClient: notion });
+
+// Custom transformer for YouTube embeds
+// fixme: this isn't working
+// n2m.setCustomTransformer("embed", async (block) => {
+//   const { embed } = block as any;
+//   if (!embed?.url) return "";
+//   const url = embed.url as string;
+//   if (!url.includes("www.youtube.com")) return "";
+//   return `<iframe src="${url}"></iframe>`;
+// });
+
+/**
+ * Type predicate for Notion response types
+ * 
+ * @param response 
+ * @returns 
+ */
+function isPageObjectResponse(response: PageObjectResponse | PartialPageObjectResponse): response is PageObjectResponse {
+  return (response as PageObjectResponse).properties !== undefined;
+}
+
+export async function getPublishedPosts(): Promise<PageObjectResponse[]> {
+  const postsObject = await notion.databases.query({
+    database_id: notionDatabaseId,
+    filter: {
+      and: [
+        {
+          property: "Published On",
+          date: {
+            is_not_empty: true
+          }
+          // status: {
+          //   // equals: "Published"
+          // }
+        }, 
+        // Uncomment for testing specific post transformations
+        // {
+        //   property: "Title",
+        //   title: {
+        //     contains: "Digital"
+        //   }
+        // },
+        // {
+        //   property: "Tags",
+        //   multi_select: {
+        //     contains: "Poetry"
+        //   }
+        // }
+      ]
+    },
+    sorts: [
+      {
+        property: "Published On",
+        direction: "descending"
+      }
+    ]
+  });
+
+  return postsObject.results.filter(isPageObjectResponse);
+}
+
+/**
+ * Colons (and maybe other special characters) cause next-mdx-remote to throw YAML formatting exceptions
+ * @param s 
+ * @returns 
+ */
+function sanitizeYAML(s: string): string {
+  return s.replace(/:/, "&#58;")
+}
+
+/**
+ * Pull out page properties and convert to an object that can be easily converted to frontmatter. 
+ * Hardcoded to my particular Notion DB setup
+ * 
+ * @param page 
+ */
+export function pagePropertiesToFrontmatterBlob(page: PageObjectResponse): { [key: string]: string } {
+  const { properties } = page;
+  const titleProperty = properties["Title"] as TitleProperty;
+  const title = titleProperty.title[0].plain_text
+  const sanitizedTitle = sanitizeYAML(title);
+
+  const excerptProperty = properties["Excerpt"] as RichTextProperty;
+  const excerpt: string | undefined = excerptProperty.rich_text[0]?.plain_text;
+  const sanitizedExcerpt = excerpt ? sanitizeYAML(excerpt) : "";
+
+  const slug = properties["Slug"] as SelectProperty;
+  const tags = properties["Tags"] as MultiSelectProperty;
+  const status = properties["Status"] as StatusProperty;
+  const publishedOn = properties["Published On"] as DateProperty;
+  const updatedOn = properties["Updated On"] as DateProperty;
+
+  return {
+    "Title": sanitizedTitle,
+    "Excerpt": sanitizedExcerpt,
+    "Slug": slug.select.name,
+    "Tags": JSON.stringify(tags.multi_select.map((ms) => ms.name)),
+    "Status": status.status.name,
+    "Published On": publishedOn.date.start,
+    "Updated On": updatedOn.date?.start
+  }
+}
+
+export function frontmatterBlobToString(blob: { [key: string]: string }): string {
+  const str = Object.keys(blob).reduce((acc, key) => `${acc}${key}: ${blob[key]}\n`, "");
+  return `---
+${str}
+---
+`;
+}
---
+++ scripts/notionExport.ts
@@ -0,0 +1,72 @@
+import { frontmatterBlobToString, getPublishedPosts, n2m, pagePropertiesToFrontmatterBlob } from "./lib/notion";
+import { promises as fs } from "fs";
+import { replaceWeirdCharacters, uuidToPageId } from "./util/string";
+import { MdBlock } from "notion-to-md/build/types";
+import { transformMarkdown, PageDatum } from "./lib/mdTransforms";
+
+// todo: replace with top-level await
+export async function base(script: () => Promise<void>) {
+  /**
+   * Node process exits with exitCode==0 when there are still
+   * promises awaiting. We prevent this from happening by adding
+   * event-loop timer and clearing it after code finishes.
+   */
+   const i = setInterval(() => {
+    /* do nothing but prevent node process from exiting */
+  }, 1000);
+
+  try {
+    await script();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    clearInterval(i);
+  }
+
+  // revert back to correct status code, because we didn't encounter any errors
+  process.exitCode = 0;
+  process.exit();
+}
+
+const PAGES_PATH = "../src/content/posts";
+const PUBLIC_PATH = "../src/public/files";
+
+// fixme: add some error handling
+export async function exportNotionPosts() {
+  const posts = await getPublishedPosts(); 
+
+  const pageData: PageDatum[] = posts.map((post) => {
+    const pageId = uuidToPageId(post.id);
+    const frontmatterBlob = pagePropertiesToFrontmatterBlob(post);
+    const slug = frontmatterBlob.Slug;
+    const tags = frontmatterBlob.Tags;
+    const frontmatter = frontmatterBlobToString(frontmatterBlob);
+    return {
+      pageId,
+      frontmatter,
+      tags,
+      slug,
+    }
+  })
+
+  for await (const {pageId, frontmatter, tags, slug} of pageData) {
+    console.info(`Converting page slug ${slug}`);
+    const mdblocks: MdBlock[] = await n2m.pageToMarkdown(pageId);
+    const { blocks: transformedMdblocks, assets } = await transformMarkdown({slug, blocks: mdblocks, tags}, pageData);
+    const mdString = n2m.toMarkdownString(transformedMdblocks);
+    const fixedString = replaceWeirdCharacters(mdString);
+  
+    const mdxDir = `${PAGES_PATH}`;
+    await fs.mkdir(mdxDir, { recursive: true });
+    await fs.writeFile(`${mdxDir}/${slug}.mdx`, `${frontmatter}${fixedString}`); 
+    console.info("Wrote mdx file");
+    for await (const { filename, buffer } of assets) {
+      await fs.writeFile(`${PUBLIC_PATH}/${slug}-${filename}`, buffer);
+      console.info(`Wrote image buffer to '${slug}-${filename}'`);
+    }
+  }
+}
+
+base(exportNotionPosts);
+
+export {}
